0bd62b38d85452bd595fea27ca43db841c062dc5
Alexandr Shevchenko <djpestro@gmail.com>
2024-11-05 20:26:17 +0600

    update get_all_tasks and task_by_id function

--- app/routers/task.py
+++ app/routers/task.py
@@ -1,15 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from backend.db_depend import get_db
+from typing import Annotated
+from models import Task
+from schemas import CreateUser, UpdateUser
+from sqlalchemy import insert, select, update, delete
+from slugify import slugify
 
 router = APIRouter(prefix='/task', tags=['TASK'])
 
 
 @router.get('/')
-async def get_all_tasks():
-    pass
+async def get_all_tasks(db: Annotated[Session, Depends(get_db)]):
+    tasks = db.scalars(select(Task).where(Task.id != 0)).all()
+    return tasks
 
 @router.get('/task_id')
-async def task_by_id():
-    pass
+async def task_by_id(db: Annotated[Session, Depends(get_db)], task_id: int):
+    task = db.scalars(select(Task).where(Task.id == task_id)).one_or_none()
+    if task:
+        return task
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Task was not found'
+        )
+    
 
 @router.post('/create')
 async def create_task():
