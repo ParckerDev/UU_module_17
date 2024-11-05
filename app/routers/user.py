a58f26a262f605e235b23f56c6427968d47e8d4e
Alexandr Shevchenko <djpestro@gmail.com>
2024-11-05 19:31:40 +0600

    edit user_update function

--- app/routers/user.py
+++ app/routers/user.py
@@ -34,7 +34,9 @@ async def create_user(db: Annotated[Session, Depends(get_db)], create_user: Crea
         db.execute(insert(User).values(username=create_user.username,
                                        firstname=create_user.firstname,
                                        lastname=create_user.lastname,
-                                       age=create_user.age))
+                                       age=create_user.age,
+                                       slug=slugify(create_user.username)))
+        db.commit()
         return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
     else:
         raise HTTPException(
@@ -43,8 +45,20 @@ async def create_user(db: Annotated[Session, Depends(get_db)], create_user: Crea
         )
 
 @router.put('/update')
-async def update_user():
-    pass
+async def update_user(db: Annotated[Session, Depends(get_db)], user_id: int, update_user: UpdateUser):
+    user = db.scalars(select(User).where(User.id == user_id)).one()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User was not found'
+        )
+    db.execute(update(User).where(User.id == user_id).values(
+        firstname=update_user.firstname,
+        lastname=update_user.lastname,
+        age=update_user.age
+    ))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'User update is successful!'}
 
 @router.delete('/delete')
 async def delete_user():
